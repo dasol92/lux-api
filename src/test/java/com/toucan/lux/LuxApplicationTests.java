60b5cfa5392edb27ba2e4d21425398230e40e972
Dasol Kim <zoocanon92@gmail.com>
2024-06-29 02:04:56 +0900

    Initial commit

---
+++ src/test/java/com/toucan/lux/LuxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toucan.lux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
