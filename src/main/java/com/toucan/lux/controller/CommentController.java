de00f438334ecafd37de448d9af26f93506b1c1f
Dasol Kim <zoocanon92@gmail.com>
2024-08-23 01:30:17 +0900

    [feat] 댓글 등록 시 토큰으로 사용자 식별

--- src/main/java/com/toucan/lux/controller/CommentController.java
+++ src/main/java/com/toucan/lux/controller/CommentController.java
@@ -5,6 +5,7 @@ import com.toucan.lux.domain.Member;
 import com.toucan.lux.domain.Post;
 import com.toucan.lux.dto.comment.CommentDTO;
 import com.toucan.lux.dto.comment.CreateCommentReq;
+import com.toucan.lux.jwt.JwtUtil;
 import com.toucan.lux.service.CommentService;
 import com.toucan.lux.service.MemberService;
 import com.toucan.lux.service.PostService;
@@ -22,6 +23,7 @@ public class CommentController {
     private final CommentService commentService;
     private final PostService postService;
     private final MemberService memberService;
+    private final JwtUtil jwtUtil;
 
     @GetMapping("/post/{postId}")
     public ResponseEntity<List<CommentDTO>> getCommentsByPost(@PathVariable Long postId) {
@@ -32,9 +34,15 @@ public class CommentController {
     }
 
     @PostMapping("/new/{postId}")
-    public ResponseEntity<CommentDTO> createComment(@RequestBody CreateCommentReq createComment, @PathVariable Long postId) {
+    public ResponseEntity<CommentDTO> createComment(
+            @RequestHeader("Authorization") String authorizationHeader,
+            @RequestBody CreateCommentReq createComment, @PathVariable Long postId) {
+
+        String token = authorizationHeader.replace("Bearer ", "").trim();
+        String userEmail = jwtUtil.getUsername(token);
+
         Post postById = postService.getPostById(postId);
-        Member author = memberService.getMemberByEmail(createComment.getAuthorEmail());
+        Member author = memberService.getMemberByEmail(userEmail);
 
         Comment newComment = Comment.builder()
                 .post(postById)
--- src/main/java/com/toucan/lux/dto/comment/CreateCommentReq.java
+++ src/main/java/com/toucan/lux/dto/comment/CreateCommentReq.java
@@ -5,5 +5,4 @@ import lombok.Data;
 @Data
 public class CreateCommentReq {
     private String content;
-    private String authorEmail;
 }
